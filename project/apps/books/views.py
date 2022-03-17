c6755652cc82b495c7ee1de8d242861f4c727834
Jake van Biezen <jake.vanbiezen@student.hu.nl>
2022-03-17 14:25:21 +0100

    Add category filter.

--- project/apps/books/views.py
+++ project/apps/books/views.py
@@ -5,10 +5,13 @@ from .models import Books
 
 def index(request):
     search = request.GET.get('search', '')
+    category = request.GET.get('category', '')
     books = Books.objects.all()
     categories = books.values('categories').distinct()
     if (search):
-        result = books.filter(title__icontains=search)
+        result = books.filter(title__icontains=search,
+                              categories__icontains=category)
+        # result = books.filter(title__icontains=search)
         return render(request, 'index.html', {'books': result, 'categories': categories})
 
     return render(request, 'index.html', {'categories': categories})
